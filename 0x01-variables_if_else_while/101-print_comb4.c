d91ced9bf313af0bf4431763a03fe39e5ebd3e5b
metiouicherifa <metiouisherifa@gmail.com>
2023-09-10 15:07:00 -0700

    c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,6 +1,7 @@
 /*
  * File: 101-print_comb4.c
-  */
+ * Auth: Brennan D Baraban
+ */
 
 #include <stdio.h>
 
@@ -10,23 +11,30 @@
  *
  * Return: Always 0.
  */
-int main() {
-    for (int i = 0; i < 10; i++) {
-        for (int j = i + 1; j < 10; j++) {
-            for (int k = j + 1; k < 10; k++) {
-                putchar('0' + i);
-                putchar('0' + j);
-                putchar('0' + k);
+int main(void)
+{
+	int digit1, digit2, digit3;
 
-                if (i < 7) {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
+	for (digit1 = 0; digit1 < 8; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
+		{
+			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+			{
+				putchar((digit1 % 10) + '0');
+				putchar((digit2 % 10) + '0');
+				putchar((digit3 % 10) + '0');
 
-    putchar('\n');
+				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+					continue;
+				
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
 
-    return 0;
+	putchar('\n');
+
+	return (0);
 }
