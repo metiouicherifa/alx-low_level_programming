6b9b79474c3f3d7276c64ec0951adce4d8e5f9c1
metiouicherifa <metiouisherifa@gmail.com>
2023-09-10 14:31:55 -0700

    c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - A program that prints combination of all one digit numbers.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int n;
 
-	for (n = 0 ; n < 10 ; n++)
+	int num;
+
+	for (num = 0; num <= 9; num++)
 	{
-		putchar(n + '0');
-		if (n < 9)
+		putchar(num + '0');
+		if (num != 9)
 		{
 			putchar(',');
-			putchar(32);
+			putchar(' ');
 		}
 	}
-	putchar('\n');
-	return (0);
+
+putchar('\n');
+
+return (0);
 }
-
-
