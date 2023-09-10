2fc386142d3c39b6cb5902964eb5f29b9143045a
metiouicherifa <metiouisherifa@gmail.com>
2023-09-10 15:20:29 -0700

    c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,29 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
- * main - A program that prints all numbers 00-99
+ * main - Prints all possible different combinations of two digits.
+ * Numbers must be separated by ", ", and only the smallest combination
+ * is printed. Numbers should be in ascending order.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-	int digit1, digit2;
+    int digit1, digit2;
 
-	for (digit1 = 0; digit1 < 9; digit1++)
-	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
+    for (digit1 = 0; digit1 < 9; digit1++)
+    {
+        for (digit2 = digit1 + 1; digit2 <= 9; digit2++)
+        {
+            putchar('0' + digit1);
+            putchar('0' + digit2);
 
-			if (digit1 == 8 && digit2 == 9)
-			continue;
-			{				
-			putchar(',');
-			putchar(' ');
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+            if (digit1 != 8 || digit2 != 9)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+
+    putchar('\n');
+
+    return (0);
 }
+
