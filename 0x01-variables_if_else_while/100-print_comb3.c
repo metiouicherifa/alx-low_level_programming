1f899b08cb9a8f03d4a60802d4abea135bd635e5
metiouicherifa <metiouisherifa@gmail.com>
2023-09-10 14:38:08 -0700

    c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,10 +14,9 @@ int main(void)
 		putchar((num / 10) + '0');
 		putchar((num % 10) + '0');
 		if (num != 99)
-		{
+			continue;
 		putchar(',');
 		putchar(' ');
-		}
 		}
 	putchar('\n');
 	return (0);
