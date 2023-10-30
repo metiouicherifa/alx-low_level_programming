cd18c577651ead94064f7084e7fc39424e516534
metiouicherifa <metiouisherifa@gmail.com>
2023-10-30 05:49:38 -0700

    0x15. C - File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,8 +5,7 @@
 /**
  * create_file - creates a file
  * @filename: filename.
- * @text_content: content writed in the file.
- *
+ * @text_content: content writed in the file
  * Return: 1 if it success. -1 if it fails.
  */
 int create_file(const char *filename, char *text_content)
